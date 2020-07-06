2a71997a50663e080e7e2594c2c635525a08a5e0
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-07-06 12:10:34 -0400

    Update 1.Numbering rows.sql

--- PostgreSQL & Windows Functions/1.Numbering rows.sql
+++ PostgreSQL & Windows Functions/1.Numbering rows.sql
@@ -1,6 +1,23 @@
+#### Numbering rows
+
 SELECT
   *,
   -- Assign numbers to each row
-  row_number() over()AS Row_N
+  ROW_NUMBER() OVER() Row_N
 FROM Summer_Medals
 ORDER BY Row_N ASC;
+
+#### Numbering Olympic games in ascending order
+What if you want to easily query the table to see in which year the 13th Summer Olympics were held?
+
+SELECT
+  Year,
+
+  -- Assign numbers to each year
+  ROW_NUMBER() OVER() Row_N
+FROM (
+  SELECT DISTINCT Year
+  FROM Summer_Medals
+  ORDER BY Year ASC
+) AS Years
+ORDER BY Year ASC;
