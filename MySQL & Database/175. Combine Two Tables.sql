141d02abd2f262c7eca811e0008e334ff9ad1a39
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-08-17 17:07:06 -0400

    Update 175. Combine Two Tables.sql

--- MySQL & Database/175. Combine Two Tables.sql
+++ MySQL & Database/175. Combine Two Tables.sql
@@ -1,3 +1,32 @@
+SQL Schema
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| PersonId    | int     |
+| FirstName   | varchar |
+| LastName    | varchar |
++-------------+---------+
+PersonId is the primary key column for this table.
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| AddressId   | int     |
+| PersonId    | int     |
+| City        | varchar |
+| State       | varchar |
++-------------+---------+
+AddressId is the primary key column for this table.
+ 
+
+Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+
+```FirstName, LastName, City, State```
+
+
 select FirstName, LastName, City, State from Person P 
 left join Address A 
 on P.PersonId = A.PersonId;
