0f9114b1607becefe505da4f8fc54b1c0dfe77e9
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-08-17 19:30:09 -0400

    Update 177. Nth Highest Salary - dense_rank().sql

--- MySQL & Database/177. Nth Highest Salary - dense_rank().sql
+++ MySQL & Database/177. Nth Highest Salary - dense_rank().sql
@@ -1,14 +1,25 @@
-#The DENSE_RANK() is a window function that assigns a rank to each row within a partition of a result set. Unlike the RANK() function, the DENSE_RANK() function returns consecutive rank values. Rows in each partition receive the same ranks if they have the same values.
+Key: DENSE_RANK()
 
-#The syntax of the DENSE_RANK() function is as follows:
+The difference between DENSE_RANK() & RANK()
+
+The DENSE_RANK() is a window function & has 2 features
+- Assigns a rank to each row within a partition of a result set 
+- Returns "consecutive rank values"
+- "Rows in each partition" receive the "same ranks" if they have the "same values"
+
+Syntax of DENSE_RANK():
+2 features:
+DENSE_RANK() involves two:
+1. The rows of each partition - PARTITION BY
+2. A specified order - ORDER BY
+3. Rank is reset when the "partition boundary is crossed".
 
 #DENSE_RANK() OVER (
 #    [PARTITION BY partition_expression, ... ]
 #    ORDER BY sort_expression [ASC | DESC], ...
 # )
-# The DENSE_RANK() function is applied to the rows of each partition defined by the PARTITION BY clause, in a specified order, defined by ORDER BY clause. It resets the rank when the partition boundary is crossed.
 
-# Write a SQL query to get the nth highest salary from the Employee table.
+Q: Write a SQL query to get the --nth highest salary-- from the Employee table.
 
 +----+--------+
 | Id | Salary |
@@ -17,7 +28,9 @@
 | 2  | 200    |
 | 3  | 300    |
 +----+--------+
-# For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+# For example, given the above Employee table, the nth highest salary where n = 2 is 200. 
+
+** If there is no nth highest salary, then the query should return null.
 
 +------------------------+
 | getNthHighestSalary(2) |
@@ -25,7 +38,16 @@
 | 200                    |
 +------------------------+
 
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      
+  );
+END
 
+
+A:
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
