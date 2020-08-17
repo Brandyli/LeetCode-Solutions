1a5f7511848a9a1ea0d35276a551386b9dec2b1c
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-08-17 17:22:37 -0400

    Update 175. Combine Two Tables.sql

--- MySQL & Database/175. Combine Two Tables.sql
+++ MySQL & Database/175. Combine Two Tables.sql
@@ -4,9 +4,9 @@ Table: Person
 +-------------+---------+
 | Column Name | Type    |
 +-------------+---------+
-| PersonId    | int     |
-| FirstName   | varchar |
-| LastName    | varchar |
+| PersonId    | int     | --> common key
+| FirstName   | varchar | --> required info
+| LastName    | varchar | --> required info
 +-------------+---------+
 PersonId is the primary key column for this table.
 Table: Address
@@ -14,18 +14,21 @@ Table: Address
 +-------------+---------+
 | Column Name | Type    |
 +-------------+---------+
-| AddressId   | int     |
-| PersonId    | int     |
-| City        | varchar |
-| State       | varchar |
+| AddressId   | int     | --> regardless if there is an address for each of those people --> In other words, it's fine for a person doesn't have "AddressId"
+| PersonId    | int     | --> common key
+| City        | varchar | --> required info
+| State       | varchar | --> required info
 +-------------+---------+
 AddressId is the primary key column for this table.
  
 
-Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+Write a SQL query for a report that provides the following information for each person in the Person table, 
+regardless if there is an address for each of those people:
 
 ```FirstName, LastName, City, State```
 
+** If you are joining two tables and want the result set to "include unmatched rows from only one table", 
+** use a LEFT OUTER JOIN clause or a RIGHT OUTER JOIN clause.
 
 select FirstName, LastName, City, State from Person P 
 left join Address A 
