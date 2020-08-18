b3c458839f55525e1223cc022984034f57aab7bf
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-08-18 14:05:08 -0400

    Update 181. Employees Earning More Than Their Managers.sql

--- MySQL & Database/181. Employees Earning More Than Their Managers.sql
+++ MySQL & Database/181. Employees Earning More Than Their Managers.sql
@@ -15,6 +15,9 @@ For the above table, Joe is the only employee who earns more than his manager.
 
 Q：
 Given the Employee table, write a SQL query that finds out employees who earn more than their managers. 
+
+Key: duplicate 2 tables and use "join" to connect two 
+
 --> employees' salary > managers' salary
 -> managers are employees at the same time, How to identify who are employees'managers?--> if employees'ID=employees'ManagerID
 -> So we need 2 tables, one for employees, another for managers
@@ -25,7 +28,7 @@ Given the Employee table, write a SQL query that finds out employees who earn mo
 | Joe      |
 +----------+
 
-S：
+A：
 SELECT a.Name AS 'Employee' FROM Employee a JOIN Employee b --> directly join since two tables are same
 ON a.ManagerId = b.Id 
 AND a.Salary > b.Salary;
