2a0b98279a349482c5f6de201cd772b7b675fafd
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-08-17 21:33:45 -0400

    Update 180. Consecutive Numbers.sql

--- MySQL & Database/180. Consecutive Numbers.sql
+++ MySQL & Database/180. Consecutive Numbers.sql
@@ -1,5 +1,6 @@
 SQL Schema
-Write a SQL query to find all numbers that appear at least three times consecutively.
+Q: 
+Write a SQL query to find all numbers that appear "at least three times consecutively".
 
 +----+-----+
 | Id | Num |
@@ -20,9 +21,13 @@ For example, given the above Logs table, 1 is the only number that appears conse
 | 1               |
 +-----------------+
 
-# Then we can select any Num column from the above table to get the target data. 
-# However, we need to add a keyword DISTINCT because it will display a duplicated number if one number appears more than 3 times consecutively.
+Key:
+** select any Num column from the above table to get the target data
+** DISTINCT needs to be added, because it will display a duplicated number if one number appears more than 3 times consecutively.
+- beacuse we need to find all numbers tha appear "at least 3 times consecutively", 3 seperate tables are needed to query
+- ids in 3 tables should be same
 
+A:
 select distinct l1.num as ConsecutiveNums
 from logs l1, logs l2, logs l3
 where l1.id = l2.id - 1
