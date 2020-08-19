cd894adf7633a5774f3ea6946d2a5a147ef34e12
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-08-18 20:20:33 -0400

    Update 175. Combine Two Tables.sql

--- MySQL & Database/175. Combine Two Tables.sql
+++ MySQL & Database/175. Combine Two Tables.sql
@@ -22,6 +22,7 @@ Table: Address
 AddressId is the primary key column for this table.
  
 
+Q:
 Write a SQL query for a report that provides the following information for each person in the Person table, 
 regardless if there is an address for each of those people:
 
