3566d993c66373b8a6f21825cf1e5d1ac384fcb6
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-08-21 10:45:28 -0400

    Update 596. Classes More Than 5 Students.sql

--- MySQL & Database/596. Classes More Than 5 Students.sql
+++ MySQL & Database/596. Classes More Than 5 Students.sql
@@ -1,2 +1,40 @@
-select class from courses 
-group by class having count(distinct student) >= 5;
+Key: having count
+
+A:
+select class
+from Courses
+group by class
+having count(student) >=5;
+
+
+There is a table courses with columns: student and class
+
+Q:
+Please list out all classes which have more than or equal to 5 students.
+
+For example, the table:
+
++---------+------------+
+| student | class      |
++---------+------------+
+| A       | Math       |
+| B       | English    |
+| C       | Math       |
+| D       | Biology    |
+| E       | Math       |
+| F       | Computer   |
+| G       | Math       |
+| H       | Math       |
+| I       | Math       |
++---------+------------+
+Should output:
+
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+ 
+
+Note:
+The students should not be counted duplicate in each course.
