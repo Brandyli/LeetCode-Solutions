9829ca7534319c5cd66d9176ea3fe7cc52ea1a61
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-08-25 23:37:03 -0400

    Create 1070. Product Sales Analysis III.sql

---
+++ MySQL & Database/1070. Product Sales Analysis III.sql
@@ -0,0 +1,71 @@
+Key: Window funciton
+
+ROW_NUMBER() OVER (
+    [PARTITION BY partition_expression, ... ]
+    ORDER BY sort_expression [ASC | DESC], ...
+)
+
+A:
+select product_id, year as first_year, quantity, price
+from (select *, row_number() over (partition by product_id order by year) as row_rank from sales) a
+where a.row_rank = 1;
+
+
+Reference:
+https://www.sqlservertutorial.net/sql-server-window-functions/sql-server-row_number-function/
+
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+sale_id is the primary key of this table.
+product_id is a foreign key to Product table.
+Note that the price is per unit.
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key of this table.
+ 
+
+Q:
+Write an SQL query that selects the product id, year, quantity, and price for the first year of every product sold.
+
+The query result format is in the following example:
+
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+
+Result table:
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+
