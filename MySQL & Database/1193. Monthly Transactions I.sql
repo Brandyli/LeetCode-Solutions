86be7bba06e8ebe03bf9d6d603a2c682751abf21
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-09-01 10:01:04 -0400

    Create 1193. Monthly Transactions I.sql

---
+++ MySQL & Database/1193. Monthly Transactions I.sql
@@ -0,0 +1,53 @@
+key:DATE_FORMAT(date, format)
+https://www.w3schools.com/sql/func_mysql_date_format.asp
+
+Explain why approved_count uses count while approved_total_amount use sum?
+
+select 
+date_format(trans_date, '%Y-%m') as month,country, # DATE_FORMAT(date, format)
+count(id) as trans_count,
+count(if(state='approved',1,null)) as approved_count, # it counts time
+sum(amount) as trans_total_amount,
+sum(if(state='approved',amount,0)) as approved_total_amount #it sum the amount
+from Transactions
+group by 1,2; # for each month and country
+
+
+Table: Transactions
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| country       | varchar |
+| state         | enum    |
+| amount        | int     |
+| trans_date    | date    |
++---------------+---------+
+id is the primary key of this table.
+The table has information about incoming transactions.
+The state column is an enum of type ["approved", "declined"].
+ 
+Q:
+Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+
+The query result format is in the following example:
+
+Transactions table:
++------+---------+----------+--------+------------+
+| id   | country | state    | amount | trans_date |
++------+---------+----------+--------+------------+
+| 121  | US      | approved | 1000   | 2018-12-18 |
+| 122  | US      | declined | 2000   | 2018-12-19 |
+| 123  | US      | approved | 2000   | 2019-01-01 |
+| 124  | DE      | approved | 2000   | 2019-01-07 |
++------+---------+----------+--------+------------+
+
+Result table:
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
++----------+---------+-------------+----------------+--------------------+-----------------------+
+| 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
+| 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
+| 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
++----------+---------+-------------+----------------+--------------------+-----------------------+
