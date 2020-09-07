012a83145c1b9794c485aebbce6dafcafe70a654
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-09-07 09:02:50 -0400

    Create 1421. NPV Queries.sql

---
+++ MySQL & Database/1421. NPV Queries.sql
@@ -0,0 +1,96 @@
+Logic:
+# 1)
+# +------+--------+--------+
+# | id   | year   | npv    |
+# +------+--------+--------+
+
+select b.id, b.year, 
+
+# 2)find the npv of all each query of queries table.
+
+ifnull(npv,0) as npv
+
+# 3) right join tables
+from NPV n right join Queries q
+using(id,year);
+                 
+
+A:
+select id, year, ifnull(npv,0) as npv
+from NPV n right join Queries q
+using(id,year);
+                 
+
+Table: NPV
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| year          | int     |
+| npv           | int     |
++---------------+---------+
+(id, year) is the primary key of this table.
+The table has information about the id and the year of each inventory and the corresponding net present value.
+ 
+
+Table: Queries
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| year          | int     |
++---------------+---------+
+(id, year) is the primary key of this table.
+The table has information about the id and the year of each inventory query.
+ 
+
+Write an SQL query to find the npv of all each query of queries table.
+
+Return the result table in any order.
+
+The query result format is in the following example:
+
+NPV table:
++------+--------+--------+
+| id   | year   | npv    |
++------+--------+--------+
+| 1    | 2018   | 100    |
+| 7    | 2020   | 30     |
+| 13   | 2019   | 40     |
+| 1    | 2019   | 113    |
+| 2    | 2008   | 121    |
+| 3    | 2009   | 12     |
+| 11   | 2020   | 99     |
+| 7    | 2019   | 0      |
++------+--------+--------+
+
+Queries table:
++------+--------+
+| id   | year   |
++------+--------+
+| 1    | 2019   |
+| 2    | 2008   |
+| 3    | 2009   |
+| 7    | 2018   |
+| 7    | 2019   |
+| 7    | 2020   |
+| 13   | 2019   |
++------+--------+
+
+Result table:
++------+--------+--------+
+| id   | year   | npv    |
++------+--------+--------+
+| 1    | 2019   | 113    |
+| 2    | 2008   | 121    |
+| 3    | 2009   | 12     |
+| 7    | 2018   | 0      |
+| 7    | 2019   | 0      |
+| 7    | 2020   | 30     |
+| 13   | 2019   | 40     |
++------+--------+--------+
+
+The npv value of (7, 2018) is not present in the NPV table, we consider it 0.
+The npv values of all other queries can be found in the NPV table.
