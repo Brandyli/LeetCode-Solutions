65694e8521f902dae9dccc483fff1126c8de0424
Brandy Li <46945617+Brandyli@users.noreply.github.com>
2020-12-02 07:16:31 -0500

    Update 1445. Apples & Oranges.sql

--- MySQL & Database/1445. Apples & Oranges.sql
+++ MySQL & Database/1445. Apples & Oranges.sql
@@ -7,19 +7,21 @@ Logic:
 # +------------+--------------+
 # | sale_date  | diff         |
 # +------------+--------------+
-select sale_date, sum(if(fruit='apples', sold_num,-sold_num)) as diff
+select sale_date, (sum(case when fruit='apples' then sold_num else 0 end))-(sum(case when fruit='oranges' then sold_num else 0 end)) diff
 from Sales
 
 2) sold each day
 group by sale_date
+order by sale_date
 
 # order by sale_date in format ('YYYY-MM-DD')
 # no need for order by since the original order satisfies the order request
 
 A:
-select sale_date, sum(if(fruit='apples', sold_num,-sold_num)) as diff 
+select sale_date, (sum(case when fruit='apples' then sold_num else 0 end))-(sum(case when fruit='oranges' then sold_num else 0 end)) diff
 from Sales
 group by sale_date
+order by sale_date
 
 Table: Sales
 
